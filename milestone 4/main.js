269225ff5d27babba8cbef42a704991bcbcab4ba
aaaafr <anastasia.frassini@icloud.com>
2021-06-10 21:13:51 +0200

    cast attori

--- milestone 4/main.js
+++ milestone 4/main.js
@@ -6,10 +6,13 @@ const app = new Vue({
     data: {
         url: 'https://api.themoviedb.org/3/search/movie?api_key=46a2e7148cb4fb54efc7aa0f84fdd844&language=it-IT&query=',
         urlTv: 'https://api.themoviedb.org/3/search/tv?api_key=46a2e7148cb4fb54efc7aa0f84fdd844&language=it-IT&query=',
+        urlCast: 'https://api.themoviedb.org/3/search/person?api_key=46a2e7148cb4fb54efc7aa0f84fdd844&language=en-US&page=1&include_adult=false&query=',
         moviesData: "",
         tvData:"",
+        castData:"",
         search: "",
         searchTv: "",
+        error: null,
     },
 
     methods: {
@@ -23,6 +26,16 @@ const app = new Vue({
             .then(resp =>{
                 console.log(resp.data.results)
                 this.moviesData = resp.data.results
+            }).catch(e => {
+                console.error(e);
+                this.error = "sorry something went wrong !" + e;
+            })
+
+            axios.get(this.urlCast + query)
+            .then(resp => {
+                console.log(resp.data.results)
+                this.castData = resp.data.results
+                console.log(this.castData)
             })
             
         },
@@ -39,7 +52,15 @@ const app = new Vue({
                 this.tvData = resp.data.results
             })
 
+            axios.get(this.urlCast + query)
+            .then(resp => {
+                console.log(resp.data.results)
+                this.castData = resp.data.results
+                console.log(this.castData)
+            })
+
         },
+ 
 
     
 
